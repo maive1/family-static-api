e233267f959c201f3ffe7bc4fdabfa3bf01a2e36
verolarrondo <vero.larrondo.r@gmail.com>
2020-05-07 05:39:39 -0400

    first commit

--- src/main.py
+++ src/main.py
@@ -7,8 +7,7 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from models import db
-#from models import Person
+from models import db, Family
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -18,24 +17,63 @@ MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 
-# Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@app.route('/member', methods=['GET', 'POST'])
+def members():
+    body = request.get_json()
 
-    response_body = {
-        "hello": "world"
-    }
+    if request.method == 'GET':
+        if body is None:
+            raise APIException("You need to specify the request body as a json object", status_code=400)
+        else: 
+            family = Family('Doe')
+            members = family.get_all_members()
+            lucky_numbers = [member['lucky_numbers'] for member in members]
+            sum_of_lucky = sum([number for sub in lucky_numbers for number in sub])
 
-    return jsonify(response_body), 200
+            response_body = {
+                "family_name": family.last_name,
+                "members": family.get_all_members(),
+                "lucky_numbers": lucky_numbers,
+                "sum_of_lucky": str(sum_of_lucky)
+            }
+
+            return jsonify(response_body), 200
+    
+    if request.method == 'POST':
+
+        family = Family('Doe')
+        response_body = family.add_member(body)
+        return jsonify(response_body),200
+
+@app.route('/member/<int:member_id>', methods=['GET', 'DELETE'] )
+def single_member(member_id=None):
+
+    body = request.get_json()
+
+    if request.method == 'GET':
+        if body is not None:
+            family = Family('Doe')
+            response_body = family.get_member(member_id)
+
+            return jsonify(response_body), 200
+        else:
+            raise APIException('You need to specify the request body as a json object', status_code=404)
+
+    if request.method == 'DELETE':
+        family = Family('Doe')
+        family.delete_member(member_id)
+        response_body = {"msg": "member was successfully deleted"}
+        return jsonify(response_body), 200
+    
+    return "Invalid Method", 404
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,17 +1,53 @@
 from flask_sqlalchemy import SQLAlchemy
+from random import randint
 
 db = SQLAlchemy()
 
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
+class Family:
+    """We are storing the Doe family members, 
+    with their lucky numbers"""
 
-#     def __repr__(self):
-#         return '<Person %r>' % self.username
+    def __init__(self, last_name):
+        self.last_name = last_name #Doe
+        
+        self._members = [{
+            "id": self._generateId(),
+            "first_name": "John",
+            "age": 33,
+            "gender": "Male",
+            "lucky_numbers": [7,13,22]
+        },
+        {   
+            "id": self._generateId(),
+            "first_name": "Jane",
+            "age": 35,
+            "gender": "Female",
+            "lucky_numbers": [10,14,3]
+        },
+        {
+            "id": self._generateId(),
+            "first_name": "Jimmy",
+            "age": 5,
+            "gender": "Male",
+            "lucky_numbers": [1]
+        }]
 
-#     def serialize(self):
-#         return {
-#             "username": self.username,
-#             "email": self.email
-#         }
+    def _generateId(self):
+        return random.randint(0, 99999999)
+
+    def add_member(self, member):
+        self._members.append(member)
+
+    def delete_member(self, id):
+        self._members = [member for member in self._members if member['id'] != id]
+        return self._members
+
+    def update_member(self, id, member):
+        self._members = [member for member in self._members if member['id'] != id]
+
+    def get_member(self, id):
+        return  member = next(filter(lambda member: member.get('id') == id, self._members), None)
+
+
+    def get_all_members(self):
+        return self._members
